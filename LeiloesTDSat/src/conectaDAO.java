75201aeaff3915096f6ac1b57380a101279553ac
AnaHorita <147055155+AnaHorita@users.noreply.github.com>
2023-10-31 09:52:40 -0300

    commit banco

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost","root", "1311ANA2557" );
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
